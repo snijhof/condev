9bbbb91c71cf8ab517f9c9f4e115d819be441a44
Sjoerd Nijhof <sjoerdnijhof@hotmail.com>
2015-11-10 10:53:15 +0100

    second commit

---
+++ ContinuousDelivery/ContinuousDelivery.Tests/Controllers/IntegrationControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContinuousDelivery.Tests.Controllers
+{
+    [TestClass]
+    public class IntegrationControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
